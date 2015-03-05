608a5e5e2eacc89002c2c0a9a43af723ee268715
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-03-05 11:11:10 +0100

    Add gender and birth and death dates

--- ancestors.php
+++ ancestors.php
@@ -4,7 +4,7 @@
         <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/async/0.9.0/async.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.min.js"></script>
         <script type="text/javascript" src="ancestors.js"></script>
     </head>
     <body>
